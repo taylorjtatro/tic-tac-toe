25803bf1974ca24c9e27633b2754b16870474b68
taylorjtatro <taylorjtatro@gmail.com>
2020-05-04 10:04:47 -0700

    Built initial javascript workings

--- js/main.js
+++ js/main.js
@@ -1,20 +1,54 @@
 
-let i =  0;
-const playGame = e => {
-    console.log(e.target);
-
-    
-    i++;
-
-    console.log(i);
-    return i;
-
-
-
-}
+const state = {
+    player: 1,
+    usedBox: {
+        player1: [],
+        player2: []
+    },
+    scores: {
+        player1: 0,
+        player2: 0
+    }
+};
 
 
+let test = document.querySelectorAll('.box');
+let tester = Array.from(test);
+console.log(tester)
+tester.forEach(el => {
+    el.innerHTML = '';
+})
+
+document.querySelector('.grid-container').addEventListener('click', el => {
+    console.log(el.target);
+    space = el.target;
+    console.log(space.innerHTML)
+    if (state.player === 1) {
+        if (space.innerHTML.includes('')) {
+            space.innerHTML = 'X';
+            state.usedBox.player1.push(space.id)
+            console.log(state.usedBox)
+            state.player = 2;
+            console.log(state.player)
+        }
+
+    } else {
+        if (space.innerHTML.includes('')) {
+            space.innerHTML = 'O';
+            state.usedBox.player2.push(space.id)
+            console.log(state.usedBox)
+            state.player = 1;
+            console.log(state.player)
+        } 
+
+    }
+
+
+   
+
+});
 
 
 
-document.querySelector('.grid-container').addEventListener('click', playGame);
+
+//Figure out how to remove adding to the array if it already contains that value
